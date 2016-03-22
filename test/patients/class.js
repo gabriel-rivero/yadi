93ab829290cdb8af0ec35b50f494306d9e837ae7
Gabriel Rivero <gabriel.rivero.sa@gmail.com>
2016-03-22 11:19:38 -0400

    updated tests

--- test/patients/class.js
+++ test/patients/class.js
@@ -1,5 +1,9 @@
+'use strict';
+
 class Class0 {
   static get dependencies() {
     return ['a', 'b'];
   }
 }
+
+module.exports = Class0;
---
+++ test/subjects/2.js
@@ -0,0 +1,1 @@
+module.exports = str => () => `the string passed was ${str}`;
